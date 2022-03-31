18f29806962658768aa089de5b629311ee74e781
phfdonda <phfdonda@gmail.com>
2022-03-30 23:14:18 -0300

    Remove react-hook-form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useForm } from "react-hook-form";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -22,16 +18,6 @@ export default function Home() {
         <h1 className={styles.title}>Compilador de Posts para Prata e Arte</h1>
 
         <p className={styles.description}>Adicione o CSV e receba os posts</p>
-
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("firstName")} />
-          <select {...register("gender")}>
-            <option value="female">female</option>
-            <option value="male">male</option>
-            <option value="other">other</option>
-          </select>
-          <input type="submit" />
-        </form>
       </main>
     </div>
   );
